0deb82c7d96a30c8540cb94b6ddf5a57896aa01f
Pedro Ramalho <pedro_hrr@hotmail.com>
2023-10-30 18:22:08 -0300

    Product of array except self

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,13 +13,21 @@ public class Main {
 //        arr.push(10);
 //
 //        arr.printArray();
-//        int[] arr = {1,2,0,0,4};
+       int[] arr = {1,2,3,4,5};
 //
 //        MaximumSumSubarray maximumSumSubarray = new MaximumSumSubarray();
 //        int resposta  = maximumSumSubarray.maxSubArray(arr);
 //
 //        JumpGameProblem jumpGameProblem = new JumpGameProblem();
 //        System.out.println(jumpGameProblem.canJump(arr));
+
+
+
+
+        ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
+      int[] resp =   productOfArrayExceptSelf.productExcept(arr);
+
+        System.out.println(Arrays.toString(resp));
 
 
 
